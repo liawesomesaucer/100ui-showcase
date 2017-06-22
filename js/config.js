e911552d01a2b44aaaa46140e7e466fd33ddf088
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-06-22 03:55:08 -0700

    Minor changes and added config

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/**
+ * Edit only the values to the constants in this file unless you know 
+ * what you're doing
+ *
+ * Note that this requires that your image files be in the format of
+ * <Number>_<Suffix>.<ImageType> ie 00_Liaw.png, 01_Liaw.png
+ */
+
+// Replace with the image type of your UIs (what goes after the .)
+var IMAGE_TYPE = 'png';
+
+// If your UIs are titled XX_LastName
+// Replace 'Example' with 'LastName', keeping the quotes
+var IMAGE_NAME_SUFFIX = 'Example';
+
+// The number of the UIs you have (remember to count the first one, 00)
+// (This should be the highest # UI you have + 1)
+var NUM_UIS = 4;
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  // TODO: add handlers to select different types of display
+
+});
