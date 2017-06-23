43cec9b97b41ddd336413b6a207a2105a1f19983
liawesomesaucer <vincent.oddrepublic@gmail.com>
2017-06-22 17:15:29 -0700

    Showcase works

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# A static site boilerplate
-A boilerplate for static site
+# A lightweight showcase for the DailyUI Challenge
 
-## How to build SASS
-Since this is static, we must compile the SASS, so run this:
+## Setup
+
+## Development
+Using gulp, so let's set that up
 
 ```
 npm install
 gulp
 ```
+Now you can feel free to edit any of the files
--- js/main.js
+++ js/main.js
@@ -1,4 +1,68 @@
 $(document).ready(function() {
-  // TODO: add handlers to select different types of display
+  var tileBtnId = '#ui-view-tiles';
+  var listBtnId = '#ui-view-list';
+  var btnClass = '.ui-view--btn';
 
+  var renderTiles = prepRenderTiles(generateUIList(), 3, tileBtnId, btnClass);
+  var renderList = prepRenderTiles(generateUIList(), 1, listBtnId, btnClass);
+
+  $(tileBtnId).click(renderTiles);
+  $(listBtnId).click(renderList);
+
+  renderList();
+
+  $('#ui-num-days').text(NUM_UIS);
 });
+
+/**
+ * Function that generates the list of names of files (uis) to show
+ */
+function generateUIList() {
+  var res = [];
+
+  var uiStr = '';
+  for (var i = 0; i < NUM_UIS; i++) {
+    // Implicit type conversion used to stringify number
+    uiStr = ('0' + i) + '_' + IMAGE_NAME_SUFFIX + '.' + IMAGE_TYPE;
+    res.push(uiStr);
+  }
+
+  return res;
+}
+
+/**
+ * Function that returns a function that generates and renders all the image 
+ * html content
+ */
+function prepRenderTiles(uiImageList, numColumns, btnId, btnClass) {
+  var colWidth = 12 / numColumns;
+
+  /**
+   * Helper function to generate HTML for one tile
+   */
+  function generateTile(ui, i) {
+    return '' +
+      // (i+1) used due to zero-based indexing
+      '<div class="ui-card--wrapper col-md-' + colWidth + '" id="' + (i+1) + '">' +
+        '<div class="ui-card card">' +
+          '<img class="ui-card--img card-img-top" src="images/uis/' + ui + '" />' +
+          '<div class="ui-card--body card-body">' +
+            '<p>Day ' + (i+1) + '</p>' +
+          '</div>' +
+        '</div>' +
+      '</div>'; 
+  }
+
+  /**
+   * Function that generates and renders all the image html content
+   */
+  return function() {
+    var res = uiImageList.reduce(function(acc, ui, i) {
+      return acc + generateTile(ui, i);
+    }, '');
+
+    $('#ui-content').html(res);
+    $(btnClass).removeClass('active');
+    $(btnId).addClass('active');
+  }
+}
